987d13ba34af69b80228618154fb91d1cee53669
alvesluis311 <felipe7311141@gmail.com>
2024-06-11 22:01:50 -0300

    projeto 1

---
+++ Game/Game/Models/Jogo.cs
@@ -0,0 +1,24 @@
+﻿namespace Game.Models
+{
+    public class Jogo
+    {
+        public string Nome { get; set; }
+        public string Genero { get; set; }
+        public string Estudio { get; set; }
+
+        public Jogo(string nome, string genero, string estudio)
+        {
+            Nome = nome;
+            Genero = genero;
+            Estudio = estudio;
+        }
+
+        // Método para exibir informações do jogo
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Gênero: {Genero}");
+            Console.WriteLine($"Estúdio: {Estudio}");
+        }
+    }
+}
